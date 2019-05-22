951169d65bb4ae660a236e1dbc24b33bbc2f8110
Daan van Dijk <daan.van.d@gmail.com>
2019-05-22 22:36:32 +0200

    Online reading and problem description

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # weavingSolver
+Aims to solve the following weaving problem:
+
+## Problem description
+__"
+  Given is an input image, a number of available wires and specified dimensions of the desired product.
+  The weaving machine can only use a maximum number of wires -- let's say 5 -- in its weaving pattern.
+  The aim is to recreate the input image with the weaving machine with minimal distortion.
+  We can assume that a 'pixel' in the fabric gets build up from 20% blocks of wires. 
+  E.g., a 'fabric pixel' may contain 20% color from wire 1, 60% color from wire 3, and 20% color from wire 5.
+"__
+ 
+## Problem description as an LP problem
--- program.py
+++ program.py
@@ -3,7 +3,24 @@ import numpy as np
 
 print("Weaving problem")
 
+n = 5
+m = 5
+R = np.zeros((n, m))
+for i in range(n):
+    for j in range(m):
+        R[i,j] = np.random.uniform(0.0, 1.0)
+
+print("Original R matrix:")
+print(R)
+
+# now we have a collection of wire's
+numWires = 2
+w = [0.0, 1.0]
+
+eta = np.zeros((n,m))
+
 # a hello world example
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
 c = [-1, 4]
 A = [[-3, 1], [1, 2]]
 b = [6, 4]
@@ -11,3 +28,8 @@ x0_bounds = (None, None)
 x1_bounds = (-3, None)
 res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds])
 print(res)
+
+# https://stackoverflow.com/questions/26305704/python-mixed-integer-linear-programming
+
+
+# https://pythonhosted.org/PuLP/CaseStudies/a_blending_problem.html
