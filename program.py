26cad995cc0bafd1c49d3bdef54d62b8b3b8e7bb
Daan van Dijk <daan.van.d@gmail.com>
2019-06-09 22:17:36 +0200

    reading/writing images

--- program.py
+++ program.py
@@ -4,6 +4,7 @@ from shared import *
 import unittest
 import cv2
 
+
 print("Weaving problem")
 
 # configuration
@@ -11,8 +12,12 @@ n = 50 # number of pixels
 resPixel = 5 # resolution of pixels
 numWires = 8 # number of wires that can be used by the machine
 
-# generate random picture
-A = np.random.uniform(0.0, 1.0, (n,3))
+# read img
+im = cv2.imread("img/lotte")
+print("Image shape:", im.shape)
+
+# write img
+im = cv2.imwrite("img/output.png", im)
 
 def generateWires(k):
     X = np.linspace(0.0, 1.0, k)
@@ -28,4 +33,4 @@ def generateWires(k):
 w = generateWires(10)
 print("Number of wires: ", np.size(w, 0))
    
-localSearch(A, w, numWires)
+# localSearch(A, w, numWires)
